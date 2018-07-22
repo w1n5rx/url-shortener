225a014859fcc2ed64f2a8826150a9ab3851865c
Vinod Singh Ramalwan <vinod.ramalwan@stud.tu-darmstadt.de>
2018-07-23 00:13:31 +0200

    GET method added

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const shortId = require('shortid');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8888;
+const HTTP_CODE_404 = 404;
+app.use(cors());
 
-app.get('/api/hello', (req, res) => {
+var hashURLMap = {};
+
+app.get('/v1/:hash', (req, res) => {
   res.send({ express: 'Hello From Express' });
+  const hash = req.params.hash;
+  const redirectUrl = hashURLMap[hash];
+
+	if (!redirectUrl) {
+		errorResponse(res, HTTP_CODE_404, `No redirect URL for ${hash}`);
+	} else {
+		res.redirect(redirectUrl);
+	}
 });
+
+const errorResponse = (res, status, text) => {
+	res.status(status);
+	res.send({ error: text });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
