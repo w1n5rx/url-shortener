cc173344c19631b242801cdb4aad9ccbc9477afa
Vinod Singh Ramalwan <vinod.ramalwan@stud.tu-darmstadt.de>
2018-07-23 00:22:36 +0200

    POST and DELETE methods added

--- server.js
+++ server.js
@@ -5,19 +5,50 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8888;
 const HTTP_CODE_404 = 404;
+const HTTP_CODE_500 = 500;
 app.use(cors());
 
 var hashURLMap = {};
 
 app.get('/v1/:hash', (req, res) => {
   res.send({ express: 'Hello From Express' });
-  const hash = req.params.hash;
-  const redirectUrl = hashURLMap[hash];
+  let hash = req.params.hash;
+  const redirectURL = hashURLMap[hash];
 
-	if (!redirectUrl) {
+	if (!redirectURL) {
 		errorResponse(res, HTTP_CODE_404, `No redirect URL for ${hash}`);
 	} else {
-		res.redirect(redirectUrl);
+		res.redirect(redirectURL);
+	}
+});
+
+const generateShortURL = (url) => {
+	return shortId.generate();
+}
+
+app.post(`/v1/links`, (req, res) => {
+	const url = req.body.url;
+
+	let hash = generateShortURL(url);
+	hashURLMap[hash] = url;
+
+	if (!url) {
+		errorResponse(res, HTTP_CODE_500, 'No URL provided in request body');
+	} else {
+		res.send({
+			'hash': hash
+		});
+	}
+});
+
+app.delete(`/v1/links/:hash`, function(req, res) {
+	let hash = req.params.hash;
+
+	if (hashURLMap.hasOwnProperty(hash)) {
+		delete hashURLMap[hash];
+		res.end();
+	} else {
+		errorResponse(res, HTTP_CODE_500, `${hash} deletion failed`);
 	}
 });
 
